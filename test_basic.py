d625589b7ba1d143e38b16f8d807f094327f9a7b
allan murungi <mcallans25@gmail.com>
2018-05-31 07:53:31 -0700

    more tests on endpoints

--- test_basic.py
+++ test_basic.py
@@ -21,13 +21,43 @@ class BasicTests(unittest.TestCase):
         self.app = app.test_client()
         
         self.assertEqual(app.debug, False)
- 
-    
+
+    def test_signup_empty_params(self):
+        response=self.sign_up("","","");
+        self.assertEqual(response.status_code, 200)
+        assert b'you have not entered any details' in response.data
+        
+    def test_signup_missing_password1(self):
+        response=self.sgnup("p@gmail.com","qwertyuiop","");
+        self.assertEqual(response.status_code, 200)
+        assert b'second password missing' in response.data
+
+    def test_sgnup_missing_password2(self):
+        response=self.signup("p@gmail.com","","qwertyuiop");
+        self.assertEqual(response.status_code, 200)
+        assert b' password missing' in response.data
+        
+    def test_signup_missing_email(self):
+        response=self.signup("","qwertyuiop","qwertyuiop");
+        self.assertEqual(response.status_code, 200)
+        assert b'email address not given' in response.data
+        
     def test_succesful_signup(self):
         response=self.signup("p@gmail.com","qwertyuiop","qwertyuiop");
         self.assertEqual(response.status_code, 200)
-        assert b'you have signed up in succesfully' in response.data
-    
+        assert b'you have signed up in succesfully' in response.data;
+
+    def test_login_missing_email(self):
+        response=self.login("","qwertyuiop");
+        self.assertEqual(response.status_code, 200)
+        assert b'no username/email entered' in response.data
+        
+        
+    def test_login_missing_password(self):
+        response=self.login("p@gmail.com","");
+        self.assertEqual(response.status_code, 200)
+        assert b'no password given' in response.data    
+        
     def test_login_default_admin(self):
         response=self.login("admin@andela.com","default");
         self.assertEqual(response.status_code, 200)
@@ -45,10 +75,7 @@ class BasicTests(unittest.TestCase):
         assert b'you have not entered email and password' in response.data
            
 
-    def test_signup_empty_params(self):
-        response=self.sign_up("","","");
-        self.assertEqual(response.status_code, 200)
-        assert b'you have not entered any details' in response.data
+    
 
     def test_signup_existing_email(self):
         response=self.sign_up("p@gmail.com","qwertyuiop","qwertyuiop");
@@ -73,10 +100,8 @@ class BasicTests(unittest.TestCase):
         
     def log_out():
         return self.app.get('/logout',follow_redirects=True);
-        
-    # executed after each test
-    def tearDown(self):
-        pass
+
+
     def edit_request(self,req_id,requesttitle,requestdetails):
         return self.app.post('/createrequest',data=dict(req_id=req_id,requesttitle=requesttitle,requestdetails=requestdetails),follow_redirects=True);
     def get_request(self,req_id):
@@ -86,6 +111,13 @@ class BasicTests(unittest.TestCase):
         
     def create_request(self):
         return self.app.post('/createrequest',data=dict(requesttitle=requesttitle,requestdetails=requestdetails),follow_redirects=True);
+
+    
+    # executed after each test
+    def tearDown(self):
+        pass
+    
+    
  
  
 ###############
